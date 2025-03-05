b7c851fa44bc4310e9cadc3d31df329645b4c1cf
bimaadam <rinbim.dev@gmail.com>
2025-03-05 20:07:21 +0700

    rebased ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
+import { Box, VStack } from "@chakra-ui/react";
+import Features from "@/components/Features";
+import Geser from "@/components/Geser";
 import Hero from "@/components/Hero";
 
 const Home = () => {
   return (
-    <div>
-      <Hero />
-    </div>
-  )
-}
+    <VStack spacing={0} align="stretch">
+      <Box width={"auto"} height={"auto"}>
+        <Geser />
+      </Box>
+      <Box>
+        <Hero />
+      </Box>
+      <Box>
+        <Features />
+      </Box>
+    </VStack>
+  );
+};
 
 export default Home;
